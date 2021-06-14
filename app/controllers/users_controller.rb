3bb3c0e076a1d6709fa1a61031bce89355bf39c3
connorworthen <connorw220@protonmail.com>
2021-06-13 18:18:13 -0700

    validations for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,8 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.valid?
-      # @token = encode_token(user_id: @user.id)
-      # render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
+      @token = encode_token(user_id: @user.id)
+      render json: { user: UserSerializer.new(@user), jwt: @token }, status: :created
       render json: { user: UserSerializer.new(@user)}
     else
       render json: {error: "Email is already tied to an exisiting account."}
@@ -14,7 +14,7 @@ class UsersController < ApplicationController
   def login
     @user = User.find_by(username: params[:user][:email])
     if @user && @user.authenticate(params[:user][:password])
-      # token = encode_token({user_id: @user.id})
+      token = encode_token({user_id: @user.id})
       render json: { user: UserSerializer.new(@user) }, status: :accepted
     else
       render json: {error: "Email or password don't match. Please try again.", status: :unauthorized}
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   has_secure_password
-  # has_many :reviews
-  # has_one :store
-  # has_many :items, :through => :store
+
+  has_many :reviews
+  has_one :store
+  has_many :items, :through => :store
+  
+  validates :first, :presence => true
+  validates :last, :presence => true
+  validates :phone, :presence => true, :uniqueness => true
+  validates :address, :presence => true
+  validates :password, :presence => true, :length => { in: 6..20 }
+  validates :email, :presence => true, :uniqueness => true
 end
