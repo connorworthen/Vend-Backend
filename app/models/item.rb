110ab15c5280f29af81ba5aae8c29813ee278cd3
connorworthen <connorw220@protonmail.com>
2021-06-13 18:30:57 -0700

    item validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :store
+
+  validates :name, :presence => true
+  validates :description, :presence => true, :length => { maximum: 500 }
+  validates :stock, :presence => true
+  validates :price, :presence => true
+  validates :category, :presence => true
 end
