1ea28ff67eee8885273c676b9e10acbb7d1bcbb7
connorworthen <connorw220@protonmail.com>
2021-06-13 18:21:22 -0700

    store validations

--- app/models/store.rb
+++ app/models/store.rb
@@ -2,4 +2,8 @@ class Store < ApplicationRecord
   belongs_to :user
   has_many :items
   has_many :users, :through => :reviews
+
+  validates :company_name => true, :uniqueness => true
+  validates :company_email => true, :uniqueness => true
+  validates :description => true, :length => { maximum: 500 }
 end
